c5504d828515a44e89928f238052284d164c8e63
Raphae23 <raphaelomo20@gmail.com>
2026-07-04 11:53:51 -1100

    first task

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+    
+        while (guess != magicNumber)
+        {
+            Console.Write("Guess the number? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Amazing! You guessed right!");
+            }
+
+        }                    
+    
     }
 }
